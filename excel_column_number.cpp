e8b7049dfd52c4aaf965178ec091f8886428d782
Alifbhanvadiya14 <61405258+Alifbhanvadiya14@users.noreply.github.com>
2020-05-11 16:31:00 +0530

    Create excel_column_number.cpp

---
+++ excel_column_number.cpp
@@ -0,0 +1,12 @@
+int Solution::titleToNumber(string A) {
+    
+    int ans=0;
+    int j=0;
+    for(int i=A.size()-1;i>=0;i--){
+        
+        ans = ans + (A[i]-'A'+1) * pow(26,j);
+        j++;
+    }
+    
+    return ans;
+}
